b43db0113a50f2999a6e93eaf8dbc1e6b89a60ff
Brian Croom <bcroom@pivotal.io>
2015-08-08 11:30:10 -0400

    Add Appledoc to the header

--- Pod/Classes/BCBalancedMultilineLabel.h
+++ Pod/Classes/BCBalancedMultilineLabel.h
@@ -1,5 +1,11 @@
 #import <UIKit/UIKit.h>
 
+/**
+ * A simple label subclass that draws itself such that each of its
+ * lines have as close to the same length as possible. It can be
+ * used anywhere that you could use an ordinary `UILabel` by simply
+ * changing the instantiated class.
+ */
 @interface BCBalancedMultilineLabel : UILabel
 
 @end
