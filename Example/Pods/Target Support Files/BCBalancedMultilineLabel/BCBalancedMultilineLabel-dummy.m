c395ff06028828678d2598b3b083209c6c958760
Brian Croom <bcroom@pivotal.io>
2015-08-10 22:51:11 -0400

    Exclude the example app’s pods from the repo

--- Example/Pods/Target Support Files/BCBalancedMultilineLabel/BCBalancedMultilineLabel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BCBalancedMultilineLabel : NSObject
-@end
-@implementation PodsDummy_BCBalancedMultilineLabel
-@end
--- Example/Pods/Target Support Files/Pods-BCBalancedMultilineLabel-Example/Pods-BCBalancedMultilineLabel-Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BCBalancedMultilineLabel_Example : NSObject
-@end
-@implementation PodsDummy_Pods_BCBalancedMultilineLabel_Example
-@end
